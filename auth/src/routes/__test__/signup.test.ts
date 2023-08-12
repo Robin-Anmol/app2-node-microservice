4a9f331f20a70d4ef8f976d734ca088b831cb34d
Anmol Gangwar <anmolgangwar64@gmail.com>
2023-08-13 00:58:00 +0530

    testing :rocket:

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -68,7 +68,7 @@ it("it set a cookie after successfull signup", async () => {
       email: "test@test.com",
       password: "password",
     })
-    .expect(201);
+    .expect(200);
 
   expect(response.get("Set-Cookie")).toBeDefined();
 });
